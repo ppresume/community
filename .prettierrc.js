ca2a4d55fd9d93a77debadbdebb107022885dffa
Xiao Hanyu <xiaohanyu1988@gmail.com>
2023-11-03 15:41:33 +0800

    chore: enable prettier

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  importOrder: ['^[./]', '^@/(.*)$'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+}
