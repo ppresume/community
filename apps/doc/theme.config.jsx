d96e1ad27cb644fc31d601b45a20c05ff2d8dea1
Xiao Hanyu <xiaohanyu1988@gmail.com>
2024-03-07 21:09:23 +0800

    feat: init doc with nextra doc theme
    Issue: https://github.com/ppresume/ppresume/issues/61

---
+++ apps/doc/theme.config.jsx
@@ -0,0 +1,9 @@
+const themeConfig = {
+  logo: <span>PPResume</span>,
+  project: {
+    link: 'https://github.com/ppresume/community',
+  },
+  // ... other theme options
+}
+
+export default themeConfig
