4b9cb33a4b6394c1b1f4aa534c1d318eaf1afaaf
Xiao Hanyu <xiaohanyu1988@gmail.com>
2024-03-10 22:05:05 +0800

    feat: add link for chat
    Issue: https://github.com/ppresume/ppresume/issues/61

--- apps/doc/theme.config.jsx
+++ apps/doc/theme.config.jsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import { useConfig } from 'nextra-theme-docs'
+import { IconBrandDiscord, IconBrandGithub } from '@tabler/icons-react'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 const themeConfig = {
@@ -30,17 +31,25 @@ const themeConfig = {
       </>
     )
   },
-  logo: <span>PPResume</span>,
-  project: {
-    link: 'https://github.com/ppresume/community',
-  },
-  docsRepositoryBase:
-    'https://github.com/ppresume/community/tree/main/apps/doc/',
   useNextSeoProps() {
     return {
       titleTemplate: '%s – PPResume',
     }
   },
+
+  logo: <span>PPResume</span>,
+
+  chat: {
+    link: 'https://discord.gg/PzbunPPkVF',
+    icon: <IconBrandDiscord width={24} height={24} />,
+  },
+  project: {
+    link: 'https://github.com/ppresume/community',
+    icon: <IconBrandGithub width={24} height={24} />,
+  },
+
+  docsRepositoryBase:
+    'https://github.com/ppresume/community/tree/main/apps/doc/',
 }
 
 export default themeConfig
