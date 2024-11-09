341741e36357b3c60f8b400bcea50cff133ee681
Xiao Hanyu <xiaohanyu1988@gmail.com>
2024-11-09 14:50:18 +0800

    refactor: upgrade next to 14.2.17 and nextra to 3.2.3 for doc

--- apps/doc/src/pages/_app.tsx
+++ apps/doc/src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+
 import { MDXEmbedProvider } from 'mdx-embed'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <MDXEmbedProvider>
       <script
