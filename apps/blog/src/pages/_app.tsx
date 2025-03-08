b03115cdb82ba14f745f9b7c41e440836ff4f143
Xiao Hanyu <xiaohanyu1988@gmail.com>
2025-03-08 12:58:50 +0800

    chore: sunset mdx-embed with @next/third-parties
    netlify build error:
    
    ```
    unhandledRejection Error [ERR_REQUIRE_ESM]: require() of ES Module
    /opt/build/repo/node_modules/.pnpm/@mdx-js+react@3.1.0_@types+react@18.3.18_react@18.3.1/node_modules/@mdx-js/react/index.js
    from
    /opt/build/repo/node_modules/.pnpm/mdx-embed@1.1.2_@mdx-js+mdx@3.1.0_acorn@8.14.1__@mdx-js+react@3.1.0_@types+react@18.3.1_de7f518e060379f5da1387f85a6c3e46/node_modules/mdx-embed/dist/components/mdx-embed-provider/mdx-embed-provider.js
    not supported.
    ```

--- apps/blog/src/pages/_app.tsx
+++ apps/blog/src/pages/_app.tsx
@@ -2,12 +2,11 @@ import '@/styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
-import { MDXEmbedProvider } from 'mdx-embed'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MDXEmbedProvider>
+    <>
       <GoogleAnalytics trackPageViews={{ ignoreHashChange: true }} />
       <script
         defer
@@ -15,6 +14,6 @@ export default function App({ Component, pageProps }: AppProps) {
         src="https://plausible.ppresume.com/js/script.js"
       ></script>
       <Component {...pageProps} />
-    </MDXEmbedProvider>
+    </>
   )
 }
